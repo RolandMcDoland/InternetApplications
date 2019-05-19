98ce742e58e68d97da4b911feac8b26c7a403b17
Roland_McDoland <miki.franek@gmail.com>
2019-05-19 22:02:22 +0200

    Lab7 added

---
+++ Lab7/SongsApp/SongsApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SongsApp.MvcApplication" Language="C#" %>
---
+++ Lab7/SongsApp/SongsApp/Models/MusicDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SongsApp.Models
+{
+    public class MusicDbContext : DbContext
+    {
+        public MusicDbContext() : base("DefaultConnection") { }
+
+        public System.Data.Entity.DbSet<SongsApp.Models.Song> Songs { get; set; }
+
+        public System.Data.Entity.DbSet<SongsApp.Models.Genre> Genres { get; set; }
+    }
+}
